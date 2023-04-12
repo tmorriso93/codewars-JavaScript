5d9615238b725eb976309d00d1dfd54fa2e7d6f9
tmorriso93 <36183040+tmorriso93@users.noreply.github.com>
2023-04-12 16:39:39 -0400

    Add files via upload
    You will be given a number and you will need to return it as a string in Expanded Form. For example:
    
    expandedForm(12); // Should return '10 + 2'
    expandedForm(42); // Should return '40 + 2'
    expandedForm(70304); // Should return '70000 + 300 + 4'

---
+++ 6kyu-Write-Number-In-Expanded-Form/main.js
@@ -0,0 +1,17 @@
+function expandedForm(num) {
+    num = num.toString()
+    const equation = []
+    let multiplier = 1
+
+    for(let i = 1; i <= num.length; i++){
+        const digit = num[num.length - i]
+
+       digit > 0 && equation.unshift(digit*multiplier)
+
+       multiplier *= 10
+    }
+
+    return equation.join(' + ')
+  }
+
+  console.log(expandedForm(120));
