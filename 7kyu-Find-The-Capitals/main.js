91f0ccb31bc2d98ce344e999ad29f854cd1fbca6
tmorriso93 <36183040+tmorriso93@users.noreply.github.com>
2023-11-12 19:53:27 -0500

    Add files via upload
    Write a function that takes a single string (word) as an argument. The function must return an ordered list containing the indexes of all capital letters in the string.
    
    "CodEWaRs" --> [0,3,4,6]

---
+++ 7kyu-Find-The-Capitals/main.js
@@ -0,0 +1,15 @@
+// Look at a string and return the index of every Capital letter.
+// in the function I use a few methods to solve this problem
+// split('') : split the word and return a string of each letter in the word 
+// Regex: /[A-Z]/: look for characters A-Z
+// test(): after you look for characters A-Z  test() will test for a match in a string
+// and return true or false. 
+// push() at the end push the index (number) into our empty array
+
+function capitals (word) {
+    const result = [];
+    word.split('').forEach((letter, index) => /[A-Z]/.test(letter) && result.push(index))
+    return result
+} 
+
+console.log(capitals("CodEWaRs"));
